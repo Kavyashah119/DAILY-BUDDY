a93c4830af15530315e617e59697a12df854cc8b
Kavyashah119 <19it131@charusat.edu.in>
2020-10-19 21:26:47 +0530

    model class changes

--- app/src/main/java/com/kavya/dailybuddy/Note.java
+++ app/src/main/java/com/kavya/dailybuddy/Note.java
@@ -2,20 +2,23 @@ package com.kavya.dailybuddy;
 
 public class Note {
 
-    String title, subtitle,datetime, text, color;
+    String title, subtitle,datetime, text, color,imagepath,webURL,pinned;
     int id;
 
     public Note(){
 
     }
 
-    public Note(String title, String subtitle, String datetime, String text, String color, int id) {
+    public Note(String title, String subtitle, String datetime, String text, String color, int id,String imagepath,String webURL,String pinned) {
         this.title = title;
         this.subtitle = subtitle;
         this.datetime= datetime;
         this.text = text;
         this.color = color;
         this.id = id;
+        this.imagepath = imagepath;
+        this.webURL = webURL;
+        this.pinned = pinned;
     }
 
     public int getId() {
@@ -33,4 +36,10 @@ public class Note {
     public String getText() { return text; }
 
     public String getColor() { return color; }
+
+    public String getImagepath() { return imagepath; }
+
+    public String getWebURL() { return webURL; }
+
+    public String getPinned() { return pinned; }
 }
